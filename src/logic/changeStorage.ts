d9c1d6647f94ed5b0f984beab3110560ec63e5f9
Toni Roos <toniroos88@googlemail.com>
2018-08-03 22:15:31 +0200

    added comments, changed loading adorner initialization

--- src/logic/changeStorage.ts
+++ src/logic/changeStorage.ts
@@ -1,3 +1,6 @@
+/**
+ * Stores all changed tags (HTMLElements).
+ */
 export class ChangeStorage {
 
     tags: HTMLElement[] = [];
@@ -5,24 +8,43 @@ export class ChangeStorage {
     constructor() {
     }
 
+    /**
+     * Add a tag to storage.
+     * @param tag The tag to store.
+    */
     addTag(tag: HTMLElement) {
         if (this.tags.indexOf(tag) === -1) {
             this.tags.push(tag);
         }
     }
 
+    /**
+     * Returns all changed tags.
+     * @return Array of tags.
+    */
     getTags() {
         return this.tags;
     }
 
+    /**
+     * Checks that tag list isn't empty.
+     * @return True if not empty.
+    */
     anyChanges() {
         return this.tags.length > 0;
     }
 
+    /**
+     * Checks that tag list is empty.
+     * @return True if empty.
+    */
     noChanges() {
         return this.tags.length === 0;
     }
 
+    /**
+     * Remove all tags from sotrage.
+    */
     clearStorage() {
         this.tags = [];
     }
--- src/logic/loadingAdornerManager.tsx
+++ src/logic/loadingAdornerManager.tsx
@@ -1,26 +1,47 @@
 import * as React from 'react';
+import * as ReactDom from 'react-dom';
 import * as EventEmitter from 'EventEmitter3';
+import * as $ from "jquery";
 import { LoadingAdorner } from '../ui/loadingAdorner';
 
+/**
+ * Manages the appearance of an loading adorner
+*/
 class LoadingAdornerManager {
 
     ee = new EventEmitter();
     ui = <LoadingAdorner eventEmitter={this.ee} />;
 
     constructor() {
+
+        const loadingAdorner = document.createElement('div');
+        const loadingAdornerId = "loadingAdorner";
+        loadingAdorner.id = loadingAdornerId;
+        $("body").append(loadingAdorner);
+
+        ReactDom.render(this.ui, document.getElementById(loadingAdornerId));
     }
 
+    /**
+     * shows the loading adorner
+    */
     showLoadingAdorner() {
         this.ee.emit('showLoadingAdornerEvent');
     }
 
+    /**
+     * hides the loading adorner
+    */
     hideLoadingAdorner() {
         this.ee.emit('hideLoadingAdornerEvent');
     }
 
-    getView() {
-        return this.ui;
-    }
+    /**
+     * returns the rendered UI of the loading adorner
+    */
+    // getView() {
+    //     return this.ui;
+    // }
 }
 
 export const loadingAdornerManager = new LoadingAdornerManager();
